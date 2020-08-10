cfd389ecf84523532310032aad4b960444382beb
Paulo Moura <joaopaulo.034a@gmail.com>
2020-08-09 22:56:29 -0300

    finalizada quarta aula - back end

--- src/server.js
+++ src/server.js
@@ -1,3 +1,4 @@
+//Dados
 const proffys = [
     {
         name:"Diego Fernandes",
@@ -23,24 +24,73 @@ const proffys = [
     }
 ]
 
+const subjects = [
+    "Artes",
+    "Biologia",
+    "Ciências",
+    "Educação física",
+    "Física",
+    "Geografia",
+    "História",
+    "Matemática",
+    "Português",
+    "Química"
+]
+
+const weekdays = [
+    "Domingo",
+    "Segunda-feira",
+    "Terça-feira",
+    "Quarta-feira",
+    "Quinta-feira",
+    "Sexta-feira",
+    "Sábado"
+]
+
+//Funcionalidades
+function getSubject(subjectNumber){
+    const position = +subjectNumber -1
+    return subjects[position]
+}
+
 function pageLanding(req, res) {
-    return res.sendFile(__dirname + "/views/index.html")//caminho para os arquivos
+    return res.render("index.html")//caminho para os arquivos
 }
 
 function pageStudy(req, res) {
-    return res.sendFile(__dirname + "/views/study.html")
+    const filters = req.query
+    return res.render("study.html", { proffys, filters, subjects, weekdays })
 }
 
 function pageGiveClasses(req, res) {
-    return res.sendFile(__dirname + "/views/give-classes.html")
+    const data = req.query //dados do form
+
+    //verificar dados vazios
+    const isNotEmpty = Object.keys(data).length > 0
+    if(isNotEmpty){
+        data.subject = getSubject(data.subject)
+        proffys.push(data) 
+        
+        return res.redirect("/study")
+    }
+
+    return res.render("give-classes.html", { subjects, weekdays})
 }
 
+//Servidor
 const express = require('express')
 const server = express()
+
+//configurando NUNJUCKS
+const nunjucks = require('nunjucks')
+nunjucks.configure('src/views', {
+    express: server,
+    noCache: true,
+})
 
 server
 .use(express.static("public")) //usar os aquivos da pasta public
 .get("/", pageLanding) //rotas 
 .get("/study", pageStudy)
 .get("/give-classes",pageGiveClasses)
-.listen(5500)
+.listen(5500) //start do servidor
